12ed83f771584a86e1e8f62806680cb9b6416954
Jee-Yu <jeeyu96@gmail.com>
2024-01-25 11:22:55 +0100

    add steps to predictionmodal

--- app/components/modals/PredictionModal.tsx
+++ app/components/modals/PredictionModal.tsx
@@ -1,12 +1,35 @@
 'use client';
 
+import { useState } from "react";
+
 import usePredictionModal from "@/app/hooks/usePredictionModal";
 
 import Modal from "./Modal";
 
+
+// create steps
+enum STEPS {
+    PREDICTIONS = 0,
+    ERRORLOG = 1,
+}
+
 const PredictionModal = () => {
     // from /hooks/usePredictionModal.ts
     const predictionModal = usePredictionModal();   
+
+    const [step, setStep] = useState(STEPS.PREDICTIONS);
+
+
+    // move from step to step
+    const onBack = () => {
+        setStep((value) => value - 1);
+    };
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    }
+
+    
 
     return ( 
         <Modal 
